fc1f8c2e83dda9a789d3e6bafa7270f56961ddd4
Sai Sasank Vindamuri <imsasankvindamuri@gmail.com>
2024-01-02 09:14:40 +0530

    Back to Square One, Let's Try with the Pygame Library Instead.

--- snake.py
+++ snake.py
@@ -1,19 +1,3 @@
-import os
+import pygame;
 
-def print_to_screen():
-    str=''
-    for i in range(10):
-        str+="."*10+"\n"
-    print(str)
-
-
-
-
-while True:
-    print_to_screen()
-    inp=input("")
-    if inp!="":
-        break
-    else:
-        pass
-    os.system('cls')
+print("Hello, World!")

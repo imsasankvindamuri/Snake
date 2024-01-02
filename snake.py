dc6ff2f054171b4a2c4a3571d1e811021229f4b3
Sai Sasank Vindamuri <imsasankvindamuri@gmail.com>
2024-01-02 09:25:38 +0530

    Made a Screen last for a Couple of Secs

--- snake.py
+++ snake.py
@@ -1,3 +1,9 @@
-import pygame;
-
-print("Hello, World!")
+import pygame
+#C'mon Sasank, You can do this!!
+pygame.init()
+i=1
+while i<=10**10:
+    display = pygame.display.set_mode((600,800))
+    pygame.display.update()
+pygame.quit()
+quit()
